b7618ce272e6214417caa550f2b709db64dc2b68
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-03-26 10:11:03 +0600

    Fake data created

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h3>this is for products</h3>
+            </div>
+            <div className="cart-container">
+                <h3>order summary</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
